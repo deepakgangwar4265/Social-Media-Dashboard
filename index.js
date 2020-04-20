47a28d4d5fb9eac16909a91ed0a1a3be99744773
deepakgangwar4265 <56259752+deepakgangwar4265@users.noreply.github.com>
2020-04-20 11:37:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+function darkmode() {
+  document.getElementById("btn").classList.toggle("toggledark");
+  document.getElementById("tglbtn").classList.toggle("tglbtndark");
+  document.getElementById("card1").classList.toggle("card-dark");
+  document.getElementById("card2").classList.toggle("card-dark");
+  document.getElementById("card3").classList.toggle("card-dark");
+  document.getElementById("card4").classList.toggle("card-dark");
+  document.getElementById("hero").classList.toggle("hero-dark");
+  document.getElementById("main").classList.toggle("main-dark");
+  document.getElementsByClassName("followers")[0].classList.toggle("followers-dark");
+  document.getElementsByClassName("followers")[1].classList.toggle("followers-dark");
+  document.getElementsByClassName("followers")[2].classList.toggle("followers-dark");
+  document.getElementsByClassName("followers")[3].classList.toggle("followers-dark");
+  document.getElementsByClassName("heading1")[0].classList.toggle("heading1-dark");
+  document.getElementsByClassName("heading2")[0].classList.toggle("heading2-dark");
+  document.getElementsByClassName("overview")[0].classList.toggle("overview-dark");
+  document.getElementsByClassName("smlcard")[0].classList.toggle("smlcard-dark");
+  document.getElementsByClassName("smlcard")[1].classList.toggle("smlcard-dark");
+  document.getElementsByClassName("smlcard")[2].classList.toggle("smlcard-dark");
+  document.getElementsByClassName("smlcard")[3].classList.toggle("smlcard-dark");
+  document.getElementsByClassName("smlcard")[4].classList.toggle("smlcard-dark");
+  document.getElementsByClassName("smlcard")[5].classList.toggle("smlcard-dark");
+  document.getElementsByClassName("smlcard")[6].classList.toggle("smlcard-dark");
+  document.getElementsByClassName("smlcard")[7].classList.toggle("smlcard-dark");
+
+
+  document.getElementsByClassName("name2")[0].classList.toggle("name2-dark");
+  document.getElementsByClassName("name2")[1].classList.toggle("name2-dark");
+  document.getElementsByClassName("name2")[2].classList.toggle("name2-dark");
+  document.getElementsByClassName("name2")[3].classList.toggle("name2-dark");
+  document.getElementsByClassName("name2")[4].classList.toggle("name2-dark");
+  document.getElementsByClassName("name2")[5].classList.toggle("name2-dark");
+  document.getElementsByClassName("name2")[6].classList.toggle("name2-dark");
+  document.getElementsByClassName("name2")[7].classList.toggle("name2-dark");
+
+
+  document.getElementsByClassName("num")[0].classList.toggle("num-dark");
+  document.getElementsByClassName("num")[1].classList.toggle("num-dark");
+  document.getElementsByClassName("num")[2].classList.toggle("num-dark");
+  document.getElementsByClassName("num")[3].classList.toggle("num-dark");
+  document.getElementsByClassName("num")[4].classList.toggle("num-dark");
+  document.getElementsByClassName("num")[5].classList.toggle("num-dark");
+  document.getElementsByClassName("num")[6].classList.toggle("num-dark");
+  document.getElementsByClassName("num")[7].classList.toggle("num-dark");
+}
